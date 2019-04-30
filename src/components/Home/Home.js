5d95a12c4108a6b5b095b4d723feefa44a3107af
Eduard Toma <eduard.daniel.toma@gmail.com>
2019-04-30 05:45:02 +0300

    fetch API, HeroImage, SearchBar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './Home.css';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -10,22 +10,88 @@ import './Home.css';
 
 class Home extends Component {
   state = {
+    movies: [],
+    heroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
+  }
 
+  componentDidMount() {
+    this.setState({ loading: true });
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  // Search items
+  searchItems = (searchTerm) => {
+    console.log(searchTerm);
+    let endpoint = '';
+    this.setState({
+      movies: [],
+      loading: true,
+      searchTerm
+    })
+
+    if (searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
+  }
+
+  loadMoreItems = () => {
+    let endpoint = '';
+    this.setState({ loading: true });
+
+    if (this.state.searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = (endpoint) => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        })
+      })
+      .catch(error => console.error('Error:', error))
   }
 
   render() {
     return (
-
       <div className="rmdb-home">
 
-        <HeroImage />
-        <SearchBar />
+        {this.state.heroImage ?
+          <div>
+            <HeroImage
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+              title={this.state.heroImage.original_title}
+              text={this.state.heroImage.overview}
+            />
+
+            <SearchBar callback={this.searchItems} />
+
+          </div> : null}
+
         <FourColGrid />
+
         <Spinner />
         <LoadMoreBtn />
-
       </div>
-
     )
   }
 }
--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 import './SearchBar.css';
 
 class SearchBar extends Component {
   state = {
+    value: ''
+  }
 
+  timeout = null;
+
+  doSeearch = (e) => {
+    this.setState({ value: e.target.value })
+    clearTimeout(this.cleartimeout)
+
+    this.timeout = setTimeout(() => {
+      this.props.callback(this.state.value);
+    }, 800);
   }
 
   render() {
     return (
-      <div>
+      <div className="rmdb-searchbar">
+        <div className="rmdb-searchbar-content">
 
-        SearchBar
+          <FontAwesome className="rmdb-fa-search" name="search" size="2x" />
 
+          <input
+            type="text"
+            className="rmdb-searchbar-input"
+            placeholder="Search"
+            onChange={this.doSeearch}
+            value={this.state.value}
+          />
+
+          {/* </i> */}
+
+        </div>
       </div>
     );
   }
